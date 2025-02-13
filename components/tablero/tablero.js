693c613f7f7cb63977c33e5b5951f96454f2d5db
Brian Morales <parismirnov@gmail.com>
2025-02-13 13:33:32 -0600

    se crea animacion de voltear cartas

--- components/tablero/tablero.js
+++ components/tablero/tablero.js
@@ -1,10 +1,24 @@
 import { data } from "./data.js";
 
+
 function item(contenido){
     let div = document.createElement('div');
     div.className = "carta";
-    div.innerText = contenido;
-
+    
+    let front = document.createElement('div');
+    front.className = "front";
+    
+    let back = document.createElement('div');
+    back.className = "back";
+    back.innerText = contenido; // Mostrar el contenido en la parte trasera
+    
+    div.appendChild(front);
+    div.appendChild(back);
+    
+    div.addEventListener("click", () => {
+        div.classList.toggle("volteada");
+    });
+    
     return div;
 }
 
